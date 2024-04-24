5c66dfc86900d94f2518bdbb4798d05ee5209f3e
Yifan <yifanli@instance-20240423-042009.c.tensile-thinker-421017.internal>
2024-04-24 06:13:16 +0000

    mv server to root folder and add inference route

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 import inference
 
 app = Flask(__name__)
@@ -9,11 +9,15 @@ def hello():
 
 @app.route('/inference')
 def inference_image():
-    output_folder = "./output"
-    path_test = './test'
-    model_path = f'../runs/cegan/generator_1000.pt'
+    output_folder = "./outputs/server_output"
+    path_to_test = './data/test_small'
+    model_path = f'./runs/cegan/generator_1000.pt'
 
-    return "generated"
+    inference.test(output_folder, path_to_test=path_to_test, cegan=True, model_path=model_path)
+
+    return send_file('outputs/server_output/0.png', mimetype='image/jpeg')
+
+
 
 
 if __name__ == '__main__':
