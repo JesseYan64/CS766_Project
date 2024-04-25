0550180a45a14d99de5bc50179a31fd5a8348d63
ivanlee1999 <yifanli@instance-20240423-042009.c.tensile-thinker-421017.internal>
2024-04-25 04:21:45 +0000

    process image

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, send_file,request
 import inference
+from datetime import datetime
+import os
+
+
 
 app = Flask(__name__)
 
@@ -13,7 +17,26 @@ def inference_image():
     path_to_test = './data/test_small'
     model_path = f'./runs/cegan/generator_1000.pt'
 
-    inference.test(output_folder, path_to_test=path_to_test, cegan=True, model_path=model_path)
+    img_temp_folder = './tmp/'
+
+    current_time = datetime.now().time().strftime('%H_%M_%S')
+
+    image_path = img_temp_folder + current_time 
+
+    os.mkdir(image_path)
+
+
+    image_name = image_path + '/' +  current_time + '.png'
+
+
+
+    print("start downloading phot to locally")
+    photo = request.files['photo']
+    # Save the photo or perform further processing
+    photo.save(image_name)
+    print("photo saved")
+
+    inference.test(output_folder, image_path, cegan=True, model_path=model_path)
 
     return send_file('outputs/server_output/0.png', mimetype='image/jpeg')
 
