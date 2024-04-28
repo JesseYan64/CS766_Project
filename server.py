2eeac93c1f1cc91192fa5b6f2b87455b5e94b483
ivanlee1999 <yifanli@instance-20240423-042009.c.tensile-thinker-421017.internal>
2024-04-28 22:52:31 +0000

    fix: use base64 image instead of file

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask import Flask, send_file,request
 import inference_web
 from datetime import datetime
 import os
+import base64
+from PIL import Image
+from io import BytesIO
 
 
 
@@ -18,28 +21,34 @@ def inference_image():
     model_path = f'./runs/cegan/generator_1000.pt'
 
     img_temp_folder = './tmp/'
-
     current_time = datetime.now().time().strftime('%H_%M_%S')
-
     image_path = img_temp_folder + current_time 
 
-    os.mkdir(image_path)
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
 
+    image_name = os.path.join(image_path, current_time + '.jpg')
+    output_image_name = os.path.join(output_folder, current_time + '.jpg')
 
-    image_name = image_path + '/' +  current_time + '.jpg'
-    output_image_name = output_folder +  '/' +  current_time + '.jpg'
+    # Assuming the base64 string is sent via form-data with the key 'photo'
+    print('start paring data')
+    data = request.form['image']
+    # The data usually starts with a header like "data:image/png;base64,"
+    # We need to split the string on ',' to remove this header
+    header, encoded = data.split(',', 1)
+    image_data = base64.b64decode(encoded)
 
+    # Now we create an Image object and save it
+    image = Image.open(BytesIO(image_data))
+    image.save(image_name)
+    print("Photo saved locally")
 
-
-    print("start downloading phot to locally")
-    photo = request.files['photo']
-    # Save the photo or perform further processing
-    photo.save(image_name)
-    print("photo saved")
-
+    # Assuming inference_web.test is a function you've defined elsewhere
     inference_web.test(output_image_name, image_name, cegan=True, model_path=model_path)
 
-    return send_file('', mimetype='image/jpeg')
+    # Send the processed image back to the client
+    return send_file(output_image_name, mimetype='image/jpeg')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
